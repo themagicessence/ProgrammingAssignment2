7d5ac148d76a99fee432416c052f7213c269a396
Zyanya Tanahara <zyanya.tanahara@ciencias.unam.mx>
2020-09-11 22:49:29 -0500

    An attempt on the matrix functions enormously based on the vector ones.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## A pair of functions which store on the cache a matrix and its inverse
 
-## Write a short comment describing this function
+## This function creates an object which contains: a matrix and a place to
+## store its inverse (if not calculated yet, it's empty; otherwise it
+##stores the inverse )
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(z){
+          x <<-z
+          inv<<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) inv <<- inverse
+  getinv <- function() inv
+  list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
+    
 }
 
 
-## Write a short comment describing this function
+
+
+## This function calculates the inverse of x, prints it, and stores it
+##in the object created by the first function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv()
+        if(!is.null(inv)) {
+          message("Accesing cached data")
+          return(inv)
+        }
+        d <- x$get()
+        inv <- solve(d,...)
+        x$setinv(inv)
+        inv
 }
